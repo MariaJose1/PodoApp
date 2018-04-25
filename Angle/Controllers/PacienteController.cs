ba9d582d5f30d755f9ddddae637cc76420afcfc5
Maria José Muñoz <mariajose.munoz.g@hotmail.com>
2018-04-25 11:32:53 +0200

    Form Patient with first version of views Edit, Delete and Details

--- Angle/Controllers/PacienteController.cs
+++ Angle/Controllers/PacienteController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Angle.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +11,145 @@ namespace Angle.Controllers
 {
     public class PacienteController : Controller
     {
-        // GET: Paciente
-        public ActionResult Index()
+        podologiaEntities db = new podologiaEntities();
+
+        // GET: /Paciente/Index
+        public ActionResult Index() //luego pasar id paciente para buscarlo y leer los datos
+        {
+            return View(db.paciente.ToList()); //vista de paciente
+        }
+
+        // GET: /Paciente/Create
+        public ActionResult Create() //luego pasar id paciente para buscarlo y leer los datos
+        {
+            return View(); //vista de paciente
+        }
+
+        // POST
+        [HttpPost]
+        public ActionResult Create(paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.paciente.Add(paciente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(paciente);
+        }
+
+        public ActionResult Error_404()
         {
             return View();
         }
+        public ActionResult Lock()
+        {
+            return View();
+        }
+
+
+        // GET: /Paciente/Edit
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            paciente paciente = db.paciente.Find(id);
+
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(paciente);
+        }
+
+        // POST: /Paciente/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit ([Bind(Include = "idPaciente, medicacionHabitual, observacion, numeroHistoriaClinica")] paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(paciente).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(paciente);
+        }
+
+        // GET: /Paciente/Delete
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            paciente paciente = db.paciente.Find(id);
+
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(paciente);
+        }
+
+
+        // POST: /Paciente/Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(paciente).State = System.Data.Entity.EntityState.Deleted;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(paciente);
+        }
+
+
+        // GET: /Paciente/Details
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            paciente paciente = db.paciente.Find(id);
+
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(paciente);
+        }
+
+
+        // POST: /Paciente/Details
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Details(paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(paciente).State = System.Data.Entity.EntityState.Detached;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(paciente);
+        }
+
     }
+
+
+
+
 }
